f4246165536299f4533e1884cfc62ef8deaaa605
bindayne-112 <maichivy0112@gmail.com>
2025-04-07 21:11:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics, logEvent } from "firebase/analytics";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB5Gb-3gstR14H7-9noyO18YcYC1vdHXtU",
   authDomain: "tichdiemsdt-a964e.firebaseapp.com",
@@ -15,10 +12,12 @@ const firebaseConfig = {
   appId: "1:59997069598:web:a2a9c7fc7a3e4f9554e90f",
   measurementId: "G-6LBZR8895J"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// script.js
+
+// Script for handling the form submission
 document.getElementById('phoneForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Ngừng gửi form
 
@@ -29,6 +28,11 @@ document.getElementById('phoneForm').addEventListener('submit', function(event) 
     if (phone) {
         message.textContent = `Cảm ơn! Số điện thoại ${phone} đã được lưu và bạn sẽ nhận điểm.`;
         message.style.color = 'green'; // Màu xanh cho thông báo thành công
+
+        // Ghi lại sự kiện gửi thông tin số điện thoại vào Firebase Analytics
+        logEvent(analytics, 'phone_submission', {
+            phone: phone
+        });
     } else {
         message.textContent = 'Vui lòng nhập số điện thoại!';
         message.style.color = 'red'; // Màu đỏ cho thông báo lỗi
