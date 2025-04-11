0016756269ff581be1df669e6b1b1b99a27ca0da
bindayne-112 <maichivy0112@gmail.com>
2025-04-11 20:19:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function submitData() {
     alert('Vui lòng nhập số điện thoại');
     return;
   }
-  fetch("https://script.google.com/macros/s/AKfycbzVi0cF3B9fVWRCnsImD_clPLBZF0Yky-BlgSoSXTus7P2sBwupl-iQGqZlqFxAySKnww/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbzgrAJB266q718FuMZG6Cnu5pMFsh6XbnlGD8VTt1pQ4pIfftGcCdyBkoKlxyAvRPxUzw/exec", {
     method: "POST",
     mode: "no-cors",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
