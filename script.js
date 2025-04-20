8f8e7abec65d7a7fd797baf081fffe4c1ea5e950
bindayne-112 <maichivy0112@gmail.com>
2025-04-20 16:52:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ function guiTinNhanZalo(oaAccessToken, phone, message) {
 // 5. Gửi thông báo Telegram
 function sendTelegramMessage(message) {
   const telegramBotToken = '7893016048:AAEzofa6mJbPyGUPuI9JNBQKTsMhb7GPD3g'; // ✅ Token thật của bạn
-  const chatId = '734918273'; // ✅ Chat ID thật của bạn
+  const chatId = '6241484252'; // ✅ Chat ID thật của bạn
 
   const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
 
